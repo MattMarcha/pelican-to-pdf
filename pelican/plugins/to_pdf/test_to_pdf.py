216c804b2559012b10513ef46f6962b211015118
Matt Marcha <git@marcha.me>
2025-12-27 09:37:50 -1000

    build: Check wkhtmltopdf before testing

--- pelican/plugins/to_pdf/test_to_pdf.py
+++ pelican/plugins/to_pdf/test_to_pdf.py
@@ -4,16 +4,17 @@ from shutil import rmtree
 from tempfile import mkdtemp
 import unittest
 
-import to_pdf
+import pdfkit
+
 from pelican import Pelican
 from pelican.readers import MarkdownReader
 from pelican.settings import read_settings
+import to_pdf
 
 CUR_DIR = os.path.dirname(__file__)
 
 
 class TestToPDF(unittest.TestCase):
-
     def setUp(self, override=None):
         self.temp_path = mkdtemp(prefix="pelicantests.")
         settings = {
@@ -28,22 +29,34 @@ class TestToPDF(unittest.TestCase):
         self.settings = read_settings(override=settings)
         pelican = Pelican(settings=self.settings)
 
+        try:
+            pdfkit.configuration()
+            self.wk_ok = True
+        except OSError:
+            # wkhtmltopdf not found
+            self.wk_ok = False
+
         pelican.run()
 
     def tearDown(self):
         rmtree(self.temp_path)
 
     def test_pdf_exists(self):
-        self.assertTrue(
-            os.path.exists(os.path.join(self.temp_path, "my-super-post-rst.pdf"))
-        )
-        self.assertFalse(
-            os.path.exists(os.path.join(self.temp_path, "my-super-post-rst-no-pdf.pdf"))
-        )
-        if MarkdownReader.enabled:
+        if self.wk_ok:
             self.assertTrue(
-                os.path.exists(os.path.join(self.temp_path, "mysuperpdf.pdf"))
+                os.path.exists(os.path.join(self.temp_path, "my-super-post-rst.pdf"))
             )
             self.assertFalse(
-                os.path.exists(os.path.join(self.temp_path, "my-super-post-md-no-pdf.pdf"))
+                os.path.exists(
+                    os.path.join(self.temp_path, "my-super-post-rst-no-pdf.pdf")
+                )
             )
+            if MarkdownReader.enabled:
+                self.assertTrue(
+                    os.path.exists(os.path.join(self.temp_path, "mysuperpdf.pdf"))
+                )
+                self.assertFalse(
+                    os.path.exists(
+                        os.path.join(self.temp_path, "my-super-post-md-no-pdf.pdf")
+                    )
+                )
